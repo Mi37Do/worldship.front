85aa585dc79047f9d9f51e3a7777758751f97171
Mi37Do. <36308654+Mi37Do@users.noreply.github.com>
2025-03-28 16:51:02 +0100

    t

--- src/views/pages/shippementView.vue
+++ src/views/pages/shippementView.vue
@@ -131,6 +131,9 @@
                 <div v-if="useInbox.focusedShippement.pickUp_local"
                   class="w-full h-fit rounded-md border border-slate-200 overflow-hidden bg-primary/5 p-3 grid grid-cols-2 gap-3 uppercase">
                   {{ useInbox.focusedShippement.pickUp_local }}
+                  <br>
+                  -----------------
+                  {{ tempBook }}
                   <!--
                   <div class="flex flex-col gap-1">
                     <span class=" font-medium">name</span>
@@ -338,7 +341,7 @@
             <div v-for="item in useInbox.packageOptions" :key="item.id" class="w-full grid grid-cols-2 gap-4">
 
               <span class="font-semibold h-10 flex items-center">{{ item.name }} <br> ($ {{ numberFormat(item.price)
-              }})</span>
+                }})</span>
               <div class="flex justify-end">
                 <commun-switch v-if="!useInbox.focusedShippement.is_payed"
                   :enabled="useInbox.focusedShippement.option_package_ids.find(i => i.id === item.id) ? true : false"
@@ -355,7 +358,7 @@
               <span class="font-bold h-10 flex items-center">Total </span>
 
               <span class="text-right my-auto font-bold">$ {{ numberFormat(useInbox.focusedShippement.total_price_cost)
-              }}
+                }}
               </span>
             </div>
 
